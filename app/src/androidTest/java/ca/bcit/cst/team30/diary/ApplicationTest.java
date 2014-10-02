8ae5131da22f31a94061b1a797587d23565abcc8
ama <angela.ma@elasticpath.com>
2014-10-01 19:39:50 -0700

    Initialize app project with blank activity.

---
+++ app/src/androidTest/java/ca/bcit/cst/team30/diary/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ca.bcit.cst.team30.diary;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
