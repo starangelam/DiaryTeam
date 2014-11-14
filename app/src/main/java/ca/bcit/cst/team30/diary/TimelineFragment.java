49bc91385454a55791c837f62188e00259518dcd
ama <starangelam@gmail.com>
2014-11-13 23:29:23 -0800

    move delete action to long click, and remove unsed timeline.xml

--- app/src/main/java/ca/bcit/cst/team30/diary/TimelineFragment.java
+++ app/src/main/java/ca/bcit/cst/team30/diary/TimelineFragment.java
@@ -6,8 +6,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import ca.bcit.cst.team30.diary.access.EntryDataSource;
 import ca.bcit.cst.team30.diary.adapter.EntryListAdapter;
 import ca.bcit.cst.team30.diary.model.Entry;
@@ -21,9 +23,8 @@ public class TimelineFragment extends ListFragment {
 	private ArrayAdapter<Entry> adapter;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.activity_timeline, container, false);
+	public  void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 
 		dataSource = new EntryDataSource(getActivity());
 		dataSource.open();
@@ -32,16 +33,30 @@ public class TimelineFragment extends ListFragment {
 		adapter = new EntryListAdapter(getActivity(), all);
 		setListAdapter(adapter);
 
-		return rootView;
+		setItemLongClickListener();
 	}
 
 	@Override
 	public void onListItemClick(ListView listView, View view, int position, long id) {
-		final Entry entry = adapter.getItem(position);
+	}
 
-		dataSource.deleteEntry(entry);
-		adapter.remove(entry);
-		adapter.notifyDataSetChanged();
+	private void setItemLongClickListener() {
+		getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+				boolean consumedLongClick = false;
+
+				final Entry entry = adapter.getItem(position);
+				dataSource.deleteEntry(entry);
+				adapter.remove(entry);
+				adapter.notifyDataSetChanged();
+
+				Toast.makeText(getActivity(), "Entry deleted", Toast.LENGTH_SHORT).show();
+
+				return consumedLongClick;
+			}
+		});
 	}
 
 	@Override
